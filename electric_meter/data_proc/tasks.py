a826349776b0b00c18f06de20ef44d4e9fb995fa
Егор Попов <shadermx@yandex.ru>
2023-09-15 11:04:07 +0300

    Improved task behavior

--- electric_meter/data_proc/tasks.py
+++ electric_meter/data_proc/tasks.py
@@ -7,19 +7,24 @@ from .serializers import DataSerializer
 
 
 @app.task
+def get_data(address, port):
+    if settings.DEBUG:
+        path = 'http://{}:8001/{}'.format(address, port)
+    else:
+        path = 'http://{}:{}'.format(address, port)
+    try:
+        response = requests.get(path)
+    except Exception as e:
+        print('Не удаслоь получить данные:', e)
+    else:
+        serializer = DataSerializer(data=response.json())
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+
+@app.task
 def get_meter_data():
     '''Фоновая задача собирающая данные с счетчиков.'''
     meters = ElectricMeter.objects.all()
     for meter in meters:
-        if settings.DEBUG:
-            path = 'http://{}:8001/{}'.format(meter.address, meter.port)
-        else:
-            path = 'http://{}:{}'.format(meter.address, meter.port)
-        try:
-            response = requests.get(path)
-        except Exception as e:
-            print('Не удаслоь получить данные:', e)
-        else:
-            serializer = DataSerializer(data=response.json())
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
+        get_data.delay(meter.address, meter.port)
